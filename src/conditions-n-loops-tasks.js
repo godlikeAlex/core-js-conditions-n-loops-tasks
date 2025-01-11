d8b3ad6572e44718c740faa10a819fe19d34e16b
godlikeAlex <godlikedesigner@gmail.com>
2025-01-12 02:56:37 +0500

    feat: resolve shuffle char without optimization

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -386,8 +386,26 @@ function sortByAsc(/* arr */) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+
+function shuffleChar(str, iterations) {
+  let result = str;
+
+  for (let i = 0; i < iterations; i += 1) {
+    let charsToShuffle = '';
+    let strWithoutShuffle = '';
+
+    for (let j = 0; j < result.length; j += 2) {
+      strWithoutShuffle += result[j];
+
+      if (j + 1 < result.length) {
+        charsToShuffle += result[j + 1];
+      }
+    }
+
+    result = strWithoutShuffle + charsToShuffle;
+  }
+
+  return result;
 }
 
 /**
