8256bdef2ae8e6e7695f0fb0dd92cc3d2971e7f5
godlikeAlex <godlikedesigner@gmail.com>
2025-01-12 18:53:46 +0500

    feat: resolve isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -261,8 +261,14 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let palindrome = '';
+
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    palindrome += str[i];
+  }
+
+  return palindrome === str;
 }
 
 /**
